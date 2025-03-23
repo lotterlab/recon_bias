aac8aebe35a947f52973787bbbab3f14cbfe1397
Matteo Wohlrapp <matteow@lyra.dfci.harvard.edu>
2025-03-23 08:18:22 -0400

    Small adjustements to the configuration

--- train_reconstruction.py
+++ train_reconstruction.py
@@ -133,7 +133,7 @@ def main():
     )
 
     # Device configuration
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
     # Classifier
     model = ReconstructionModel()
